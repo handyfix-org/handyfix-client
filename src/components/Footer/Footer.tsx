636fa41389a7fd008afb545294169b92aac0749c
Estefck <estef.komu@gmail.com>
2023-07-21 13:57:30 -0400

    social media icons added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,22 +1,28 @@
 import "./Footer.scss";
+import Facebook from "../../../src/assets/icons/facebook.png"
+import Youtube from "../../../src/assets/icons/youtube.png"
+import Instagram from "../../../src/assets/icons/instagram.png"
+import Twitter from "../../../src/assets/icons/twitter.png"
 
 type FooterProps = {
   logo: string, 
-  title: string, 
+  features: string, 
   paragraph: string, 
 }
 
-const Footer : React.FC<FooterProps> = ({logo, title, }) =>{
+const Footer : React.FC<FooterProps> = ({logo, features, }) =>{
   return <footer className="footer">
-    <p>{title} Product</p>
-    <p>{title} Features</p>
-    <p>{title} Resources</p>
+    <p className="footer__feature">{features} Product</p>
+    <p className="footer__feature">{features} Features</p>
+    <p className="footer__feature">{features} Resources</p>
     <h3>{logo} HANDYFIX</h3>
-    <h3>{title} About</h3>
-    <h3>{title} Blog</h3>
-    <h3>{title} Support</h3>
-
-
+    <p className="footer__feature">{features} About</p>
+    <p className="footer__feature">{features} Blog</p>
+    <p className="footer__feature">{features} Support</p>
+    <img className="footer__icon" src={Facebook} alt="facebook-icon" />
+    <img className="footer__icon" src={Youtube} alt="facebook-icon" />
+    <img className="footer__icon" src={Instagram} alt="facebook-icon" />
+    <img className="footer__icon" src={Twitter} alt="facebook-icon" />
   </footer>
  
 }
