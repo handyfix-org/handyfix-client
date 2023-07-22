1cb6cbe23e8bd254625dfa18c0d17bae3faeb9d0
Estefck <estef.komu@gmail.com>
2023-07-21 21:50:40 -0400

    link added to the Handyfix logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,10 @@
 import "./Footer.scss";
+import { Link } from 'react-router-dom';
 import Facebook from "../../../src/assets/icons/facebook.png"
 import Youtube from "../../../src/assets/icons/youtube.png"
 import Instagram from "../../../src/assets/icons/instagram.png"
 import Twitter from "../../../src/assets/icons/twitter.png"
+
 
 type FooterProps = {
   logo: string, 
@@ -16,7 +18,9 @@ const Footer : React.FC<FooterProps> = ({logo, features, paragraph}) =>{
     <p className="footer__feature-title">{features} Product</p>
     <p className="footer__feature-title">{features} Features</p>
     <p className="footer__feature-title">{features} Resources</p>
+    <Link to="/" className="footer__feature-logo">
     <h3>{logo} HANDYFIX</h3>
+    </Link>
     <p className="footer__feature-title">{features} About</p>
     <p className="footer__feature-title">{features} Blog</p>
     <p className="footer__feature-title">{features} Support</p>
