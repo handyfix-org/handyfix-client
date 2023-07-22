81ead5c36a9fbe46392a2fe101394e5f66902aa9
Estefck <estef.komu@gmail.com>
2023-07-21 21:42:32 -0400

    footer including anchor tags for the icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -22,10 +22,18 @@ const Footer : React.FC<FooterProps> = ({logo, features, paragraph}) =>{
     <p className="footer__feature-title">{features} Support</p>
     </div>
     <section className="footer__icon">
-    <img className="footer__icon-facebook" src={Facebook} alt="facebook-icon" />
-    <img className="footer__icon-youtube" src={Youtube} alt="facebook-icon" />
-    <img className="footer__icon-instagram" src={Instagram} alt="facebook-icon" />
-    <img className="footer__icon-twitter" src={Twitter} alt="facebook-icon" />
+      <a href="https://www.facebook.com/">
+        <img className="footer__icon-facebook" src={Facebook} alt="facebook-icon" />
+      </a>
+      <a href="https://www.youtube.com/">
+        <img className="footer__icon-youtube" src={Youtube} alt="facebook-icon" />
+    </a>
+    <a href="https://www.instagram.com/">
+        <img className="footer__icon-instagram" src={Instagram} alt="facebook-icon" />
+    </a>
+    <a href="https://twitter.com/">
+        <img className="footer__icon-twitter" src={Twitter} alt="facebook-icon" />
+    </a>
     </section>
     <p className="footer__copyright">{paragraph} © 2023 HANDYFIX INC. </p>
   </footer>
