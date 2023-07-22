7b243c485d6d69dae4f40bb1b8ef372a6ccf6d66
Estefck <estef.komu@gmail.com>
2023-07-21 20:43:34 -0400

    footer with styling for desktop version

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,22 +7,27 @@ import Twitter from "../../../src/assets/icons/twitter.png"
 type FooterProps = {
   logo: string, 
   features: string, 
-  paragraph: string, 
+  paragraph: number, 
 }
 
-const Footer : React.FC<FooterProps> = ({logo, features, }) =>{
+const Footer : React.FC<FooterProps> = ({logo, features, paragraph}) =>{
   return <footer className="footer">
-    <p className="footer__feature">{features} Product</p>
-    <p className="footer__feature">{features} Features</p>
-    <p className="footer__feature">{features} Resources</p>
+    <div className="footer__feature">
+    <p className="footer__feature-title">{features} Product</p>
+    <p className="footer__feature-title">{features} Features</p>
+    <p className="footer__feature-title">{features} Resources</p>
     <h3>{logo} HANDYFIX</h3>
-    <p className="footer__feature">{features} About</p>
-    <p className="footer__feature">{features} Blog</p>
-    <p className="footer__feature">{features} Support</p>
-    <img className="footer__icon" src={Facebook} alt="facebook-icon" />
-    <img className="footer__icon" src={Youtube} alt="facebook-icon" />
-    <img className="footer__icon" src={Instagram} alt="facebook-icon" />
-    <img className="footer__icon" src={Twitter} alt="facebook-icon" />
+    <p className="footer__feature-title">{features} About</p>
+    <p className="footer__feature-title">{features} Blog</p>
+    <p className="footer__feature-title">{features} Support</p>
+    </div>
+    <section className="footer__icon">
+    <img className="footer__icon-facebook" src={Facebook} alt="facebook-icon" />
+    <img className="footer__icon-youtube" src={Youtube} alt="facebook-icon" />
+    <img className="footer__icon-instagram" src={Instagram} alt="facebook-icon" />
+    <img className="footer__icon-twitter" src={Twitter} alt="facebook-icon" />
+    </section>
+    <p className="footer__copyright">{paragraph} © 2023 HANDYFIX INC. </p>
   </footer>
  
 }
